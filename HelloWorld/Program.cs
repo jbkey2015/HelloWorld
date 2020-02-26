637cea4346345a821dea16b1d4276c2047550f8a
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-02-25 19:58:01 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Lion", "Toucan", "Cat", "Dog", "Mouse" };
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            var numOfVowels = 0;
+            var lastIsLetterE = false;
+
+            foreach (var animal in animals)
+            {
+                var animalLength = animal.Length;
+                var lastLetter = animal[animalLength - 1];
+                foreach (var letter in animal)
+                {
+                    foreach (char vowel in vowels)
+                    {
+                        if (vowel == letter)
+                        {
+                            numOfVowels++;
+                            break;
+                        }
+                        if (lastLetter == 'e')
+                        {
+                            lastIsLetterE = true;
+                        }
+
+                    }
+
+                }
+
+                if (numOfVowels >= 2 && !lastIsLetterE)
+                {
+                    Console.WriteLine(animal);
+                }
+
+                numOfVowels = 0;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
